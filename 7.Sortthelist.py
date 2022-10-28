41136de6a000a991939f0c1b2e8c305b5059813d
oykukaplan <oykukaplan01@gmail.com>
2022-10-28 19:27:05 +0300

    hw

---
+++ 7.Sortthelist.py
@@ -0,0 +1,12 @@
+# a list of numbers
+my_numbers = [10, 8, 3, 22, 33, 7, 11, 100, 54]
+
+#sort list in-place in ascending order
+my_numbers.sort()
+
+#print modified list
+print(my_numbers)
+
+#output
+
+#[3, 7, 8, 10, 11, 22, 33, 54, 100]
